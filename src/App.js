4d490698079c2271276e7e9a6e8cd1cf5fda7bef
arminl17 <armin_lokvancic@hotmail.com>
2021-07-26 19:42:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav />
+    <Banner />
+    <Row title="NETFLIX ORIGINALS"  fetchUrl = {requests.getNetflixOriginals} isLargeRow />
+    <Row title="Trending Now" fetchUrl = {requests.getTrending} />
+    <Row title="Top Rated" fetchUrl = {requests.getTopRated} />
+    <Row title="Action movies" fetchUrl = {requests.getActionMovies} />
+    <Row title="Comedy movies" fetchUrl = {requests.getComedyMovies} />
+    <Row title="Romance movies" fetchUrl = {requests.getRomanceMovies} />
+    <Row title="Horror movies" fetchUrl = {requests.getHorrorMovies} />
+    <Row title="Documentaries" fetchUrl = {requests.getDocumentaries} />
+
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,29 @@
+import React, { Component, useEffect, useState } from 'react';
+import "./Nav.css";
+
+
+function Nav() {
+    const [show, handleShow] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if(window.scrollY > 100) {
+                handleShow(true);
+            }else handleShow(false);
+        });
+        return () => {window.removeEventListener("scroll");}
+    }, [])
+    return(
+        <div className ={`nav ${show && "nav-black"}`}>
+            <img className = "nav-logo"
+            src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+            alt = "Netflix logo" />
+
+            <img className = "nav-avatar"
+            src = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt = "User picture" />
+        </div>
+
+    )
+}
+
+export default Nav;
